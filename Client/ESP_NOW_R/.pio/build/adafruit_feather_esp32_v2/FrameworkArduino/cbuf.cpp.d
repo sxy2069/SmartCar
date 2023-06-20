534e4949c05c97e1a46fb37087247ad5a03d776e
sxy2069 <sxy2069@163.com>
2023-06-21 01:35:49 +0800

    “update”

---
+++ Client/ESP_NOW_R/.pio/build/adafruit_feather_esp32_v2/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/adafruit_feather_esp32_v2/FrameworkArduino/cbuf.cpp.o: \
+ C:/Users/sxy20cdd/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/sxy20cdd/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
---
+++ Client/ESP_NOW_R/.pio/build/adafruit_feather_esp32_v2/FrameworkArduino/firmware_msc_fat.c.d
@@ -0,0 +1,3 @@
+.pio/build/adafruit_feather_esp32_v2/FrameworkArduino/firmware_msc_fat.c.o: \
+ C:/Users/sxy20cdd/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
+ C:/Users/sxy20cdd/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
